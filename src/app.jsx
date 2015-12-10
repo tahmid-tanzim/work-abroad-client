fb63d39915a7e620a2b730b8e491f2b7df72eddc
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-12-10 18:23:22 +0600

    Reducer refactored

--- src/app.jsx
+++ src/app.jsx
@@ -4,11 +4,11 @@ import Router from 'react-router';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import io from 'socket.io-client';
+
 import reducer from './reducer';
 import {setState} from './actions/application';
 import remoteActionMiddleware from './remote_action_middleware';
 import routes from './routes';
-
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state =>
